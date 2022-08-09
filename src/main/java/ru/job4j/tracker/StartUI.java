01798b6a2bc9d98a72ac4b4bbddfac19108d907e
greenman777 <chaos777@mail.ru>
2022-08-09 21:36:39 +0300

    9. Написать тесты на StartUI. [#785]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -23,8 +23,8 @@ public class StartUI {
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(), new ShowAllAction(), new EditAction(), new DeleteAction(),
-                new FindByIdAction(), new FindByNameAction(), new ExitAction()
+                new CreateAction(), new ShowAllAction(), new ReplaceAction(), new DeleteAction(),
+                new FindByIdAction(), new FindByNameAction(), new Exit()
         };
         new StartUI().init(input, tracker, actions);
     }
