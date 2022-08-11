f9fd01e148b074f57e9c009581835cf1d7a03d42
greenman777 <chaos777@mail.ru>
2022-08-11 08:30:24 +0300

    0.3. Тестирование исключений с junit [#219366]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -2,7 +2,6 @@ package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static org.assertj.core.api.Assertions.assertThat;
