e81ffe94edc8f520ed8d3398f94d8c27a994afeb
greenman777 <chaos777@mail.ru>
2022-08-16 15:12:14 +0300

    1. Singleton [#94606 #332541]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker;
 
 public class SingleTracker {
     private static SingleTracker instance = null;
+    private Tracker tracker = new Tracker();
 
     private SingleTracker() {
     }
@@ -12,8 +13,6 @@ public class SingleTracker {
         }
         return instance;
     }
-
-    private Tracker tracker = new Tracker();
 
     public Item add(Item item) {
         return tracker.add(item);
