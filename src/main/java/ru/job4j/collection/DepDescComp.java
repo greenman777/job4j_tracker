fc9f844d4f6ffd276cf2cb90608308ece23142cf
Dmitry Khodyrev <chaos777@mail.ru>
2022-09-17 14:57:35 +0300

    2. Отсортировать департаменты [#34352] mod compare method

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -7,6 +7,7 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String o1, String o2) {
         String o1First = o1.split("/")[0];
         String o2First = o2.split("/")[0];
-        return o1First.equals(o2First) ? o1.compareTo(o2) : o2.compareTo(o1);
+        int rsl = o2First.compareTo(o1First);
+        return rsl != 0 ? rsl : o1.compareTo(o2);
     }
 }
