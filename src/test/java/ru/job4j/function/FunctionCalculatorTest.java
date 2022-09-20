04e520d2035ba43334fc71bb4322d47e49ddef42
Dmitry Khodyrev <chaos777@mail.ru>
2022-09-20 09:27:06 +0300

    5. Подсчет функции в диапазоне. [#24251 #344984] add Test for Exponential Function and Quadratic Function

--- src/test/java/ru/job4j/function/FunctionCalculatorTest.java
+++ src/test/java/ru/job4j/function/FunctionCalculatorTest.java
@@ -15,4 +15,20 @@ public class FunctionCalculatorTest {
         List<Double> expected = Arrays.asList(11D, 13D, 15D);
         assertThat(result).containsAll(expected);
     }
+
+    @Test
+    public void whenExponentialFunction() {
+        FunctionCalculator function = new FunctionCalculator();
+        List<Double> result = function.diapason(2, 6, x -> Math.pow(2, x));
+        List<Double> expected = Arrays.asList(4D, 8D, 16D, 32D);
+        assertThat(result).containsAll(expected);
+    }
+
+    @Test
+    public void whenQuadraticFunction() {
+        FunctionCalculator function = new FunctionCalculator();
+        List<Double> result = function.diapason(1, 7, x -> 3 * x * x + 2 * x - 1);
+        List<Double> expected = Arrays.asList(4D, 15D, 32D, 55D, 84D, 119D);
+        assertThat(result).containsAll(expected);
+    }
 }
